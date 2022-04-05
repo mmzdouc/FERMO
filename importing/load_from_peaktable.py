bce8085739d08857b8e03fe43348f296fdfb5941
Niek de Jonge <76995965+niekdejonge@users.noreply.github.com>
2022-04-05 11:23:58 +0200

    add enters

--- importing/load_from_peaktable.py
+++ importing/load_from_peaktable.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-
+ 
 def load_from_peaktable(df):
     """Reads MzMine3-style peaktable as dataframe.
     
@@ -11,7 +11,7 @@ def load_from_peaktable(df):
     columns for easier retrieval further downstream.
     """
     peaktable = pd.read_csv(df, sep=',')
-    
+     
     #tests for correct peaktable format"
     try: 
         peaktable[["row ID", "row m/z", "row retention time"]]
@@ -20,9 +20,10 @@ def load_from_peaktable(df):
         print("'row ID', 'row m/z' or 'row retention time' not found.")
         print("Check format of peak table and try again.")
         return None
-    
+     
     #renames sample columns for easier handling
     for old_col_name in peaktable.filter(regex="Peak").columns:
         new_col_name = old_col_name.rsplit(" ")[0]
         peaktable.rename(columns={old_col_name:new_col_name}, inplace=True)
     return peaktable
+
