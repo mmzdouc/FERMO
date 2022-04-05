afe9b908b52f2836570268ea2254f0889055b6de
Niek de Jonge <76995965+niekdejonge@users.noreply.github.com>
2022-04-05 11:23:08 +0200

    add enters

--- misc_funct/samples_listing.py
+++ misc_funct/samples_listing.py
@@ -4,7 +4,6 @@ import pandas as pd
 def samples_listing(peaktable):
     """Gets sample names + feature precence from peaktable:
     
-    
     Scrapes sample names from MzMine2/3 style peak table.
     Extracts feature IDs of features that were found in each sample.
     Returns a dict with samplename:list of features
@@ -17,3 +16,4 @@ def samples_listing(peaktable):
                 features_in_sample.append(int(row["row ID"]))
         samples[entry] = features_in_sample
     return samples
+
