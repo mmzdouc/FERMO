a941805fd4e39f9f7dfe506d4497e6574fdaa396
habic001 <sarah.habicht@wur.nl>
2023-03-13 21:04:04 +0100

    Display file overview table on loading page
    - add functions (adjusted from dash_fermo) to parse the uploaded session file,
    compare version of file with current FERMO-version, create a dictionary with
    relevant info, create placeholder dictionary and connect all the functions
    - display an 'empty' placeholder table when no file was loaded_file
    - give feedback according to file version
    - change html files to create table dynamically based on the file content
    - style the table according to the dash-equivalent

--- src/fermo/app_utils/input_testing.py
+++ src/fermo/app_utils/input_testing.py
@@ -1,12 +1,11 @@
 from flask import (
     flash,
-    redirect,
     current_app,  # allows to access the config elements
     request,
-    url_for
 )
 from werkzeug.utils import secure_filename
 import os
+import json
 
 
 def allowed_file(filename):
@@ -25,27 +24,33 @@ def allowed_file(filename):
         current_app.config.get('ALLOWED_EXTENSION')
 
 
-def check_file(inputID, file_format):
-    '''
+def save_file(inputID, file_format):
+    '''Check file for validity and save it
 
     Parameters
     ----------
     inputID: 'str'
     file_format: 'str'
 
+    Return
+    ------
+    'str': filename if user input was valid,
+    none otherwise
+
     Notes
     -----
     Checks if the file was succesfully transmitted via request, if a filename
-    provided, and if it has the right format.
+    provided, and if it has the right format. If so, the file is saved in the
+    location specified in config.toml under 'UPLOAD_FOLDER'.
     inputID must be taken from the corresponding html file.
     '''
     if inputID not in request.files:
         flash('Something went wrong, please try again')
-        return redirect(request.url)
+        return None
     file = request.files[inputID]
     if file.filename == '':
         flash('No file was loaded. Please upload a session-file.')
-        return redirect(request.url)
+        return None
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         if filename.endswith(file_format):
@@ -53,12 +58,177 @@ def check_file(inputID, file_format):
                 file.save(os.path.join(current_app.config.get(
                     'UPLOAD_FOLDER'), filename))
             except FileNotFoundError:
-                print("file or folder didn't exist, \
+                print("File or folder didn't exist, \
                         so the uploaded file couldn't be saved")
-            return redirect(url_for('views.inspect_uploaded_file',
-                                    filename=filename))
+            return filename
         else:
             flash(f"File must be a {file_format}-file!")
-            return redirect(request.url)
+            return None
     else:
         print('something went wrong with the file upload of element:', inputID)
+        return None
+
+
+def check_version(content_dict, filename, version):
+    '''Compare version of uploaded file with current FERMO-version
+
+    Parameters
+    ----------
+    content_dict: 'dict' from opened json file
+
+    Return
+    ------
+    'str': Error/warning message if versions are(may) not (be) compatible,
+    none otherwise
+    '''
+    uploaded_version = content_dict['FERMO_version']
+    uploaded_version_split = uploaded_version.split('.')
+    major, minor, fix = (
+        uploaded_version_split[0],
+        uploaded_version_split[1],
+        uploaded_version_split[2],
+        )
+    if major != version.split('.')[0] or \
+       minor != version.split('.')[1]:
+        return f'''❌ Error: The loaded session file "{filename}"
+        has been created using FERMO version {uploaded_version}.
+        This is incompatible with the currently running version
+        of FERMO ({version}). Please re-process your data or
+        use another version of FERMO.'''
+    elif fix != version.split('.')[2]:
+        return f'''❗ Warning: The loaded session file "{filename}"
+        has been created using FERMO version {uploaded_version}. The
+        currently running version of FERMO is {version}.
+        While these versions should be compatible, there might be
+        some unforseen behavior of the application.'''
+    else:
+        return None
+
+
+def empty_loading_table():
+    '''Generate placeholder table for loading page
+
+    Returns
+    -------
+    list of lists: placeholder pre-file-upload
+    '''
+    content = [
+        ['Date of creation', None],
+        ['Time of creation', None],
+        ['FERMO version', None],
+        ['-----', '-----'],
+        ['Filename: peaktable', None],
+        ['Filename: MS² data', None],
+        ['Filename: group metadata', None],
+        ['Filename: quantitative biological data', None],
+        ['Filename: user-library', None],
+        ['-----', '-----'],
+        ['Mass deviation', None],
+        ['Minimum number of fragments per MS² spectrum', None],
+        ['QuantData factor', None],
+        ['Blank factor', None],
+        ['Relative intensity filter range', None],
+        ['Spectral similarity networking algorithm', None],
+        ['Fragment similarity tolerance', None],
+        ['Spectrum similarity score cutoff', None],
+        ['Maximum number of links to each feature in spectral network', None],
+        ['Minimum number of matched peaks in spectral similarity matching',
+         None],
+        ['MS2Query used', None],
+        ['MS2Query blank annotation', None],
+        ['MS2Query relative intensity range', None],
+        ['-----', '-----'],
+        ['Process log step', 'Description'],
+        ]
+    return content
+
+
+def create_session_table(
+    params,
+    files,
+    metadata,
+    version,
+    logging
+):
+    '''Parse session file and generate overview table
+
+    Parameters
+    ----------
+    params : `dict`
+    files : `dict`
+    metadata : `dict`
+    version : `dict`
+    logging : `dict`
+
+    Returns
+    -------
+    list of lists: session file info
+    '''
+    content = [
+        ['Date of creation', metadata['date']],
+        ['Time of creation', metadata['time']],
+        ['FERMO version', version],
+        ['-----', '-----'],
+        ['Filename: peaktable', files['peaktable_name']],
+        ['Filename: MS² data', files['mgf_name']],
+        ['Filename: group metadata', files['metadata_name']],
+        ['Filename: quantitative biological data',
+            files['bioactivity_name']],
+        ['Filename: user-library', files['user_library_name']],
+        ['-----', '-----'],
+        ['Mass deviation', params['mass_dev_ppm']],
+        ['Minimum number of fragments per MS² spectrum',
+            params['min_nr_ms2']],
+        ['QuantData factor', params['bioact_fact']],
+        ['Blank factor', params['column_ret_fact']],
+        ['Relative intensity filter range',
+            '-'.join([str(i) for i in params[
+                'relative_intensity_filter_range']])],
+        ['Spectral similarity networking algorithm',
+            params['spec_sim_net_alg']],
+        ['Fragment similarity tolerance', params['spectral_sim_tol']],
+        ['Spectrum similarity score cutoff',
+            params['spec_sim_score_cutoff']],
+        ['Maximum number of links to each feature in spectral network',
+            params['max_nr_links_ss']],
+        ['Minimum number of matched peaks in spectral similarity matching',
+            params['min_nr_matched_peaks']],
+        ['MS2Query used', params['ms2query']],
+        ['MS2Query blank annotation', params['ms2query_blank_annotation']],
+        ['MS2Query relative intensity range',
+            '-'.join([str(i) for i in params['ms2query_filter_range']])],
+        ['-----', '-----'],
+        ['Process log step', 'Description'],
+        ]
+    for entry in logging:
+        content.append([entry, logging[entry]])
+    return content
+
+
+def parse_sessionfile(filename, version):
+    '''Check version compatibility and create session file
+
+    Parameters
+    ----------
+    filename: 'str'
+    version: 'str'; current version of FERMO as read from __version__.py file
+
+    Return
+    ------
+    'dict': containing the parsed file info
+    'str': message for (possible) incompatibility or Errors
+    '''
+    try:
+        with open(os.path.join(current_app.config.get('UPLOAD_FOLDER'),
+                               filename)) as f:
+            content_dict = json.load(f)
+            table_dict = create_session_table(content_dict['params_dict'],
+                                              content_dict['input_filenames'],
+                                              content_dict['session_metadata'],
+                                              content_dict['FERMO_version'],
+                                              content_dict['logging_dict'])
+            message = check_version(content_dict, filename, version)
+            return table_dict, message
+    except FileNotFoundError as e:
+        print(e)
+        return empty_loading_table(), 'FileNotFoundError'
--- src/fermo/views.py
+++ src/fermo/views.py
@@ -1,11 +1,17 @@
 from flask import (
     Blueprint,
+    flash,
+    redirect,
     render_template,
     request,
+    url_for,
 )
 import fermo.__version__ as __version__
 
-from fermo.app_utils.input_testing import check_file
+from fermo.app_utils.input_testing import (
+    save_file,
+    parse_sessionfile,
+    empty_loading_table)
 views = Blueprint(__name__, "views")
 
 
@@ -18,20 +24,30 @@ def landing(version=__version__.__version__):
 @views.route("/loading", methods=['GET', 'POST'])
 def loading(version=__version__.__version__):
     if request.method == 'POST':
-        return check_file('sessionFile', 'json')
-    return render_template('loading.html', version=version)
+        filename = save_file('sessionFile', 'json')
+        if filename:
+            return redirect(url_for('views.inspect_uploaded_file',
+                                    filename=filename))
+        else:
+            return redirect(request.url)
+    return render_template('loading.html', version=version,
+                           table=empty_loading_table())
 
 
 @views.route("/loading/<filename>")  # for inspecting the uploaded file before submitting it to the dashboard.
 def inspect_uploaded_file(filename, version=__version__.__version__):
-    return render_template('loaded_file.html', version=__version__.__version__)
+    table_dict, message = parse_sessionfile(filename, version)
+    if message:
+        flash(message)
+    return render_template('loaded_file.html', version=version,
+                           table=table_dict)
 
 
 @views.route("/dashboard")
 def dashboard(version=__version__.__version__):
-    return render_template('dashboard.html', version=__version__.__version__)
+    return render_template('dashboard.html', version=version)
 
 
 @views.route("/processing", methods=['GET', 'POST'])
 def processing(version=__version__.__version__):
-    return render_template('processing.html', version=__version__.__version__)
+    return render_template('processing.html', version=version)
