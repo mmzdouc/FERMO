2d44fca779ef610ea34422d51ff8c9a7b1af2bca
habic001 <sarah.habicht@wur.nl>
2023-03-13 09:14:49 +0100

    Change config file to config toml-format
    - delete config.py and create config.toml
    - add toml to requirements.txt
    - import toml to app.py and 'read' config file accordingly

--- src/fermo/app.py
+++ src/fermo/app.py
@@ -1,13 +1,14 @@
 # external imports
 from flask import Flask
+import toml
 
 # internal file imports
 from views import views
 
 
-# initialize the app 
+# initialize the app
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
+app.config.from_file('config.toml', load=toml.load)
 app.register_blueprint(views, url_prefix="/")
 
 if __name__ == '__main__':
