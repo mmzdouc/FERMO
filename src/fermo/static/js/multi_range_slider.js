08d8f40f774cd09c71c15c1da8b43da5de013ed2
habic001 <sarah.habicht@wur.nl>
2023-03-29 15:49:54 +0200

    Refactor fillSlider() and add docstrings
    fillSlider() was refactored to not ask for the 'toSlider' twice just under a different name (the second instance was called 'controlSlider').
    Docstrings were mostly added to fully selfwritten functions as opposed
    to the ones taken from the link at the top of the file.

--- src/fermo/static/js/multi_range_slider.js
+++ src/fermo/static/js/multi_range_slider.js
@@ -1,4 +1,4 @@
-// code taken and adjusted from https://medium.com/@predragdavidovic10/native-dual-range-slider-html-css-javascript-91e778134816
+// code until line 87 was taken and adjusted from https://medium.com/@predragdavidovic10/native-dual-range-slider-html-css-javascript-91e778134816
 
 function controlFromInput(fromSlider, fromInput, toInput, controlSlider) {
     const [from, to] = getParsed(fromInput, toInput);
@@ -10,7 +10,7 @@ function controlFromInput(fromSlider, fromInput, toInput, controlSlider) {
         fromSlider.value = from;
     }
 }
-    
+
 function controlToInput(toSlider, fromInput, toInput, controlSlider) {
     const [from, to] = getParsed(fromInput, toInput);
     fillSlider(fromInput, toInput, '#C6C6C6', '#116789', controlSlider);
@@ -53,11 +53,18 @@ function getParsed(currentFrom, currentTo) {
   return [from, to];
 }
 
-function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
-    const rangeDistance = (to.max-to.min) / to.step;
-    const fromPosition = (from.value - to.min) / to.step;
-    const toPosition = (to.value - to.min) / to.step;
-    controlSlider.style.background = `linear-gradient(
+/**
+ * Control coloring of the slider depending on the selected range
+ * @param {string} fromSlider  - HTML-ID of the slider that controls the lower bound
+ * @param {string} toSlider    - HTML-ID of the slider that controls the upper bound
+ * @param {string} sliderColor - Hexcode for the color of the unselected part of the slider
+ * @param {string} rangeColor  - Hexcode for the color of the selected range
+ */
+function fillSlider(fromSlider, toSlider, sliderColor, rangeColor) {
+    const rangeDistance = (toSlider.max-toSlider.min) / toSlider.step;
+    const fromPosition = (fromSlider.value - toSlider.min) / toSlider.step;
+    const toPosition = (toSlider.value - toSlider.min) / toSlider.step;
+    toSlider.style.background = `linear-gradient(
       to right,
       ${sliderColor} 0%,
       ${sliderColor} ${(fromPosition)/(rangeDistance)*100}%,
@@ -67,9 +74,11 @@ function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
       ${sliderColor} 100%)`;
 }
 
+/**
+ * Keep 'toSlider' at the bottom of the stack when the two sliders overlap
+ * to avoid interferance with the input elements
+*/
 function setToggleAccessible(currentTarget) {
-  /**Keep toSlider at the bottom of the 'stack' when the two sliders overlap
-   * to avoid interferance with the input elements */
   if (Number(currentTarget.value) <= 0 ) {
     currentTarget.style.zIndex = 2;
   } else {
@@ -77,12 +86,16 @@ function setToggleAccessible(currentTarget) {
   }
 }
 
-function checkURL(pagename) {
-  /** Check from which URL the script was called */
-  var pageurl = window.location.href;
-  var pg = pageurl.split("/");
-  var pgname = (pg[pg.length - 1]); // access last element of the URL
-  if (pagename == pgname) /*check whether the current page is {pagename}*/
+/**
+ * Check if script was called from desired page and return boolean value
+ * 
+ * @param {string} desiredPage - Name of the desired page (return true if script was called from this page)
+*/
+function checkURL(desiredPage) {
+  var pageurl = window.location.href; // get the URL of the current page
+  var splitUrl = pageurl.split("/");
+  var currentPage = (splitUrl[splitUrl.length - 1]); // access last element of the URL
+  if (desiredPage == currentPage) /*check whether the current page is {pagename}*/
   {
     return true;
   } else {
@@ -90,8 +103,14 @@ function checkURL(pagename) {
   }
 }
 
+/**
+ * Initialize the multi-range-Slider 
+ * @param {string} fromSliderID - HTML-ID of the slider that controls the lower bound
+ * @param {string} toSliderID   - HTML-ID of the slider that controls the upper bound
+ * @param {string} fromInputID  - HTML-ID of the input element that controls the lower bound
+ * @param {string} toInputID    - HTML-ID of the input element that controls the upper bound
+*/
 function initSlider(fromSliderID, toSliderID, fromInputID, toInputID) {
-  /** Initialize the multi-range-Slider */
   const fromSlider = document.querySelector(fromSliderID);
   const toSlider = document.querySelector(toSliderID);
   const fromInput = document.querySelector(fromInputID);
@@ -106,11 +125,14 @@ function initSlider(fromSliderID, toSliderID, fromInputID, toInputID) {
 }
 
 
-// call functions with correct parameters depending on the page it was called from
+
+
+// Call functions with correct parameters depending on the page it was called from
 if(checkURL("processing")){
 
   // Slider for relative intensity filter (processing page)
   initSlider('#fromSlider', '#toSlider', '#fromInput', '#toInput');
+
   // Slider for MS2Query relative intensity filter (processing page)
   initSlider('#fromSliderMS2Q', '#toSliderMS2Q', '#fromInputMS2Q', '#toInputMS2Q');
 
@@ -118,8 +140,10 @@ if(checkURL("processing")){
 
   // Slider for Novelty Score (dashboard page)
   initSlider('#fromSliderNovel', '#toSliderNovel', '#fromInputNovel', '#toInputNovel');
+
   // Slider for Relative intensity (dashboard page)
   initSlider('#fromSliderRelInt', '#toSliderRelInt', '#fromInputRelInt', '#toInputRelInt');
+
   // Slider for Peak overlap (dashboard page)
   initSlider('#fromSliderPeak', '#toSliderPeak', '#fromInputPeak', '#toInputPeak');
 }
