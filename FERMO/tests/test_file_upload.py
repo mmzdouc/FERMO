a724b77cc0723b3cddcf8d33d6fbdad8bc3e540c
habic001 <sarah.habicht@wur.nl>
2023-06-05 14:34:27 +0200

    Fix test because cwd changed

--- FERMO/tests/test_file_upload.py
+++ FERMO/tests/test_file_upload.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 def test_upload_folder_exists():
     '''Test if the example file exists'''
-    example_file = Path.cwd() / 'example_data/FERMO_session.json'
+    example_file = Path.cwd() / 'FERMO/example_data/FERMO_session.json'
     assert example_file.is_file()
