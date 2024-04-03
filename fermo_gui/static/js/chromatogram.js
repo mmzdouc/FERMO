0aafc6487669967fa47adee9a4cc8ecdd3d61d89
Hannah Augustijn <h.e.augustijn@gmail.com>
2024-04-03 13:55:14 +0200

    Feature table structure

--- fermo_gui/static/js/chromatogram.js
+++ fermo_gui/static/js/chromatogram.js
@@ -7,8 +7,15 @@ document.addEventListener('DOMContentLoaded', function() {
     var firstSample = document.querySelector('.select-sample');
     if (firstSample) {
         var firstSampleName = firstSample.getAttribute('data-sample-name');
-        visualizeData(firstSampleName, statsChromatogram);
+        sampleData = getSampleData(firstSampleName, statsChromatogram)
+        visualizeData(sampleData);
         document.getElementById('activeSample').textContent = 'Sample: ' + firstSampleName;
+
+        // Update the feature table
+        chromatogramElement.on('plotly_click', function(data) {
+            var featureId = data.points[0].data.name;
+            updateTableWithFeatureData(featureId, sampleData);
+        });
     }
 
     // Activate the clicked sample of the 'Sample overview'
@@ -16,59 +23,41 @@ document.addEventListener('DOMContentLoaded', function() {
     rows.forEach(function(row) {
        row.addEventListener('click', function() {
           var sampleName = this.getAttribute('data-sample-name');
-          visualizeData(sampleName, statsChromatogram);
+          sampleData = getSampleData(sampleName, statsChromatogram)
+          visualizeData(sampleData);
           document.getElementById('activeSample').textContent = 'Sample: ' + sampleName;
+
+          // Update the feature table
+          chromatogramElement.on('plotly_click', function(data) {
+              var featureId = data.points[0].data.name;
+              updateTableWithFeatureData(featureId, sampleData);
+          });
        });
     });
+
 });
 
-function visualizeData(sampleName, statsChromatogram) {
-    // Extract sample data for plotting chromatogram lines
-    var activeSampleData = statsChromatogram[sampleName];
-    const traceInt = activeSampleData.map(obj => obj.trace_int);
-    const traceRt = activeSampleData.map(obj => obj.trace_rt);
-
-    // Extract sample data for tooltip
-    const featureId = activeSampleData.map(obj => obj.f_id);
-    const absInt = activeSampleData.map(obj => obj.abs_int);
-    const relInt = activeSampleData.map(obj => obj.rel_int);
-    const retTime = activeSampleData.map(obj => obj.rt);
-    const precMz = activeSampleData.map(obj => obj.mz);
-
-    // Get the max and min RT across all samples to use as plot range
-    const allTraceRtValues = Object.values(statsChromatogram).
-    flatMap(sample => sample.flatMap(obj => obj.trace_rt));
-    const maxRt = Math.max(...allTraceRtValues);
-    const minRt = Math.min(...allTraceRtValues);
-    const upperRange = maxRt + maxRt * 0.02;
-    const lowerRange = minRt - minRt * 0.05;
+function visualizeData(sampleData) {
 
     // Fill or line colors of chromatogram peaks and legend
-    var fillColors = ['#99bf9f', '#fce097'];
+    var fillColors = ['rgba(153, 191, 159, 0.75)', '#fce097'];
     var lineColors = ['#5a755e', '#fab80f'];
     var legendLabels = ['Selected', 'Blank'];
 
-    // Data plotting
     var data = [];
-    for ( var i = 0 ; i < traceRt.length ; i++ ) {
-
-      var toolTip = `&nbsp;Feature ID: ${featureId[i]}<br>
-                     Precursor m/z: ${precMz[i]}<br>
-                     Retention time: ${retTime[i]}<br>
-                     Relative intensity: ${relInt[i]}<br>
-                     Absolute intensity: ${absInt[i]}<br>`
-
+    for ( var i = 0 ; i < sampleData.traceRt.length ; i++ ) {
       var result = {
         showlegend: false,
-        x: traceRt[i],
-        y: traceInt[i],
+        x: sampleData.traceRt[i],
+        y: sampleData.traceInt[i],
         type: 'scatter',
         mode: 'lines',
-        text: toolTip,
+        name: `${sampleData.featureId[i]}`,
+        text: getToolTip(sampleData, i),
         hoverinfo: 'text',
         hoverlabel: { bgcolor: '#41454c' },
         fill: 'toself',
-        fillcolor: '#99bf9f',
+        fillcolor: 'rgba(153, 191, 159, 0.75)',
         line: {
           color: '#5a755e',
           width: 2,
@@ -108,7 +97,7 @@ function visualizeData(sampleName, statsChromatogram) {
         autorange: false,
         showgrid: false,
         visible: true,
-        range: [lowerRange, upperRange]
+        range: sampleData.upLowRange
       },
       yaxis: {
         autorange: false,
@@ -127,4 +116,47 @@ function visualizeData(sampleName, statsChromatogram) {
       },
     };
     Plotly.newPlot('mainChromatogram', data, layout);
+}
+
+function getSampleData(sampleName, statsChromatogram) {
+    // Extract sample data for plotting chromatogram lines
+    var activeSampleData = statsChromatogram[sampleName];
+
+    // Get the max and min RT across all samples to use as plot range
+    const allTraceRtValues = Object.values(statsChromatogram).
+    flatMap(sample => sample.flatMap(obj => obj.trace_rt));
+    const maxRt = Math.max(...allTraceRtValues);
+    const minRt = Math.min(...allTraceRtValues);
+
+    return {
+        traceInt: activeSampleData.map(obj => obj.trace_int),
+        traceRt: activeSampleData.map(obj => obj.trace_rt),
+        featureId: activeSampleData.map(obj => obj.f_id),
+        absInt: activeSampleData.map(obj => obj.abs_int),
+        relInt: activeSampleData.map(obj => obj.rel_int),
+        retTime: activeSampleData.map(obj => obj.rt),
+        precMz: activeSampleData.map(obj => obj.mz),
+        upLowRange: [minRt - minRt * 0.05, maxRt + maxRt * 0.02]
+    }
+}
+
+function getToolTip(sampleData, peakNumber) {
+    return `&nbsp;Feature ID: ${sampleData.featureId[peakNumber]}<br>
+            Precursor m/z: ${sampleData.precMz[peakNumber]}<br>
+            Retention time: ${sampleData.retTime[peakNumber]}<br>
+            Relative intensity: ${sampleData.relInt[peakNumber]}<br>
+            Absolute intensity: ${sampleData.absInt[peakNumber]}<br>`
+}
+
+function updateTableWithFeatureData(fId, sampleData) {
+    // Update the general feature info table with the clicked feature data
+    for ( var i = 0 ; i < sampleData.featureId.length ; i++ ) {
+        if (sampleData.featureId[i] == fId) {
+            document.getElementById('featureIdCell').textContent = sampleData.featureId[i];
+            document.getElementById('precMzCell').textContent = sampleData.precMz[i];
+            document.getElementById('retTimeCell').textContent = sampleData.retTime[i];
+            document.getElementById('relIntCell').textContent = sampleData.relInt[i];
+            document.getElementById('absIntCell').textContent = sampleData.absInt[i];
+        }
+    }
 }
