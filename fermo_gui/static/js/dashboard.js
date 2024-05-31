ea924b814be3a474fc2fa33c89a4c256c50046d0
Hannah Augustijn <h.e.augustijn@gmail.com>
2024-05-31 12:40:41 +0200

    Add drag and drop setting

--- fermo_gui/static/js/dashboard.js
+++ fermo_gui/static/js/dashboard.js
@@ -2,82 +2,27 @@ import { getSampleData, getFeatureData } from './parsing.js';
 import { updateFeatureTables, hideTables } from './dynamic_tables.js';
 import { visualizeData } from './chromatogram.js';
 import { visualizeNetwork, hideNetwork } from './network.js';
+import { enableDragAndDrop, disableDragAndDrop } from './dragdrop.js';
 
 document.addEventListener('DOMContentLoaded', function() {
     var dragged;
+    // Initial state of drag and drop based on checkbox
+    var allowDragAndDropCheckbox = document.getElementById('allowDragAndDrop');
+    if (allowDragAndDropCheckbox.checked) {
+        enableDragAndDrop();
+    } else {
+        disableDragAndDrop();
+    }
 
-    // Drag and Drop functionality
-    document.addEventListener("drag", function(event) {}, false);
-
-    document.addEventListener("dragstart", function(event) {
-        dragged = event.target;
-        // make it half transparent
-        event.target.style.opacity = .5;
-        event.dataTransfer.setData("draggedAccId", event.target.id);
-        setTimeout(() => event.target.classList.toggle("hidden"));
-        // change dropzone background when hovering
-        if (event.target.classList.contains("dropzone")) {
-            event.target.style.background = "#a3a3a3";
+    // Toggle drag and drop functionality based on checkbox state
+    allowDragAndDropCheckbox.addEventListener('change', function() {
+        if (this.checked) {
+            enableDragAndDrop();
+        } else {
+            disableDragAndDrop();
         }
-        // highlight dropzones
-        document.querySelectorAll('.dropzone').forEach(function(dropzone) {
-            dropzone.classList.add('dropzone-dragging');
-        });
-    }, false);
+    });
 
-    document.addEventListener("dragend", function(event) {
-        // reset the transparency
-        event.target.style.opacity = "";
-        // reset dropzone background
-        document.querySelectorAll('.dropzone').forEach(function(dropzone) {
-            dropzone.classList.remove('dropzone-dragging');
-        });
-    }, false);
-
-    document.addEventListener("dragover", function(event) {
-        // prevent default to allow drop
-        event.preventDefault();
-    }, false);
-
-    document.addEventListener("dragenter", function(event) {
-        // highlight potential drop target when the draggable element enters it
-        if (event.target.classList.contains("dropzone")) {
-            event.target.style.background = "#a3a3a3";
-        }
-    }, false);
-
-    document.addEventListener("dragleave", function(event) {
-        // reset background of potential drop target when the draggable element leaves it
-        if (event.target.classList.contains("dropzone")) {
-            event.target.style.background = "";
-        }
-    }, false);
-
-    document.addEventListener("drop", function(event) {
-        event.preventDefault();
-
-        // move dragged elem to the selected drop target
-        if (event.target.classList.contains("dropzone")) {
-            event.target.style.background = "";
-
-            // Get the ID of the dragged accordion from the data transfer object
-            const draggedAccId = event.dataTransfer.getData("draggedAccId");
-            const draggedAcc = document.getElementById(draggedAccId);
-            const fromContainer = draggedAcc.parentNode;
-            const toContainer = event.target;
-
-            // Check if the drop zone is already occupied, if so swap containers, if not append
-            if (toContainer !== fromContainer) {
-                const existingAcc = toContainer.firstElementChild;
-                if (existingAcc) {
-                    fromContainer.appendChild(existingAcc);
-                }
-                toContainer.appendChild(draggedAcc);
-            } else {
-                toContainer.appendChild(draggedAcc);
-            }
-        }
-    }, false);
 
     // Load all data
     var chromatogramElement = document.getElementById('mainChromatogram');
@@ -150,6 +95,4 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('networkSelect').addEventListener('change', handleNetworkTypeChange);
         });
     });
-
-
 });
