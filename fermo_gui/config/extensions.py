8d6316919a562058ef4b1dd03b463e047bf8b4bb
Mitja M. Zdouc <mmzdouc@gmail.com>
2024-06-13 08:35:50 +0200

    Removed flask-session

--- fermo_gui/config/extensions.py
+++ fermo_gui/config/extensions.py
@@ -20,10 +20,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+
 from flask_mail import Mail
-from flask_session import Session
 from flask_socketio import SocketIO
 
 mail = Mail()
-session = Session()
 socketio = SocketIO()
